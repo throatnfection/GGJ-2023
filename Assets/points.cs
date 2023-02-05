2ee46f84a88ab5140430111d381c97e847c1039e
throatnfection <shemysleezygirl@gmail.com>
2023-02-05 06:12:13 -0800

    points

--- Assets/points.cs
+++ Assets/points.cs
@@ -4,46 +4,87 @@ using UnityEngine;
 
 public class points : MonoBehaviour
 {
-    int naturePoints;
-    int treeCost;
-    int tsunamiCost;
-    int tornadoCost;
-    int thunderCost;
-    int maxTornado;
-    int maxTree;
-    int maxTsunami;
-    int maxThunder;
+    float naturePoints;
+    float treeCost;
+    float tsunamiCost;
+    float tornadoCost;
+    float thunderCost;
     int tornadoCount;
     int treeCount;
     int tsunamiCount;
     int thunderCount;
+    float newNaturePoints;
+    public float coolDownTime = 10f;
+    private float nextUseTime;
+
+    int newMaxTornado;
+    int newMaxTree;
+    int newMaxTsunami;
+    int newMaxThunder;
 
     void Start()
     {
         naturePoints = 35;
-        treeCost = 0;
+        treeCost = 1;
         tsunamiCost = 8;
         tornadoCost = 5;
         thunderCost = 5;
-        maxTornado = 2;
-        maxTree = 2;
-        maxTsunami = 1;
-        maxThunder = 2;
         tornadoCount = 0;
         treeCount = 0;
         tsunamiCount = 0;
         thunderCount = 0;
     }
 
+    public void PrintMax() {
+      float maxTornado;
+      float maxTree;
+      float maxTsunami;
+      float maxThunder;
+      maxTornado = Random.Range(1, (int)((naturePoints / tornadoCost) / 2) + 1);
+      newNaturePoints = naturePoints - (maxTornado * tornadoCost);
+      maxTree = Random.Range(1, (int)((((newNaturePoints / 4) * 3) / treeCost) + 1));
+      newNaturePoints = newNaturePoints - (maxTree * treeCost);
+      maxTsunami = Random.Range(1, (int)((newNaturePoints) / tsunamiCost) + 1);
+      newNaturePoints = newNaturePoints - (maxTsunami * tsunamiCost);
+      maxThunder = newNaturePoints / thunderCost;
+      if (maxThunder < 0) {
+        PrintMax();
+        return;
+      }
+      newMaxTornado = (int)Mathf.Ceil(maxTornado);
+      newMaxTree = (int)Mathf.Ceil(maxTree);
+      newMaxTsunami = (int)Mathf.Ceil(maxTsunami);
+      newMaxThunder = (int)Mathf.Ceil(maxThunder);
+      Debug.Log("tornado: " + newMaxTornado + " tree: " + newMaxTree + " tsunami: " + newMaxTsunami + " thunder: " + newMaxThunder);
+      Debug.Log(newMaxTornado * tornadoCost + newMaxTree * treeCost + newMaxTsunami * tsunamiCost + newMaxThunder * thunderCost);
+    }
+
+    public bool notOnCooldown() {
+      if (Time.time > nextUseTime)
+      {
+          nextUseTime = Time.time + coolDownTime;
+          return true;
+      } else {
+        return false;
+      }
+    }
+
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.H))
+      if (Input.GetKeyDown(KeyCode.M)) {
+        PrintMax();
+      }
+      /*if (Input.GetKeyDown(KeyCode.H))
       {
         if (naturePoints > treeCost) {
           if (treeCount < maxTree) {
-            Debug.Log("tree placed");
-            treeCount = treeCount + 1;
-            naturePoints = naturePoints + 1;
+            if (notOnCooldown() == true) {
+              Debug.Log("tree placed");
+              treeCount = treeCount + 1;
+              naturePoints = naturePoints + 1;
+            } else {
+              Debug.Log("on cooldown");
+            }
           } else {
             Debug.Log("max tree se postaveni");
           }
@@ -55,9 +96,13 @@ public class points : MonoBehaviour
       {
         if (naturePoints > tsunamiCost) {
           if (tsunamiCount < maxTsunami) {
-            naturePoints = naturePoints - tsunamiCost;
-            Debug.Log("tsunami placed");
-            tsunamiCount = tsunamiCount + 1;
+            if (notOnCooldown() == true) {
+              naturePoints = naturePoints - tsunamiCost;
+              Debug.Log("tsunami placed");
+              tsunamiCount = tsunamiCount + 1;
+            } else {
+              Debug.Log("on cooldown");
+            }
           } else {
             Debug.Log("max tsunami se postaveni");
           }
@@ -69,9 +114,13 @@ public class points : MonoBehaviour
       {
         if (naturePoints > tornadoCost) {
           if (tornadoCount < maxTornado) {
-            naturePoints = naturePoints - tornadoCost;
-            Debug.Log("tornado placed");
-            tornadoCount = tornadoCount + 1;
+            if (notOnCooldown() == true) {
+              naturePoints = naturePoints - tornadoCost;
+              Debug.Log("tornado placed");
+              tornadoCount = tornadoCount + 1;
+            } else {
+              Debug.Log("on cooldown");
+            }
           } else {
             Debug.Log("max tornado se postaveni");
           }
@@ -83,15 +132,19 @@ public class points : MonoBehaviour
       {
         if (naturePoints > thunderCost) {
           if (thunderCount < maxThunder) {
-            naturePoints = naturePoints - thunderCost;
-            Debug.Log("thunderstorm placed");
-            thunderCount = thunderCount + 1;
+            if (notOnCooldown() == true) {
+              naturePoints = naturePoints - thunderCost;
+              Debug.Log("thunderstorm placed");
+              thunderCount = thunderCount + 1;
+            } else {
+              Debug.Log("on cooldown");
+            }
           } else {
             Debug.Log("max thunderstorms se postaveni");
           }
         } else {
           Debug.Log("nemas dovolno poeni");
         }
-      }
+      } */
     }
 }
